58fc22ead5f45edf2bbfe89654762411b04aa3a9
KETAM Djillali <ketam.djalal@gmail.com>
2017-10-05 10:19:30 +0200

    sequelette du projet

---
+++ Compte/src/main/java/fr/uvsq/uvsq21608184/App.java
@@ -0,0 +1,13 @@
+package fr.uvsq.uvsq21608184;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
